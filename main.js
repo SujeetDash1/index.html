26aacca39524557e4add09a23f1be0eecd427646
SujeetDash1 <121320338+SujeetDash1@users.noreply.github.com>
2024-07-12 16:40:22 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+    fabric.imagefromURL(get_image, function(Img)) }
+
+    }
+    fabric.image.fromURL('BirthdayImage.jpg', function(Img) {
+    block_image_object = Img;
+    
+    block_image_object.scaleToWidth(700);
+    block_image_object.scaleToHeight(510);
+    block_image-object.set({
+        top:0,
+        left:0
+        });
+        canvas.add(block_image-object);
+        });	
+	
+}
+
+function playSound(){
+	x,play();
+}
+
+
+
+
+
